80741a5d75ce05de36f6df1779ca84de372a880d
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-11-15 13:21:40 +0100

    typescript classes wrap up

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,8 @@ class Department {
         this.name = name;
         this.employees = [];
     }
-    describe() {
-        console.log(`Departmnent ${this.id}: ${this.name}`);
+    static createEmployee(name) {
+        return { name: name };
     }
     addEmployee(employee) {
         this.employees.push(employee);
@@ -15,28 +15,55 @@ class Department {
         console.log(this.employees);
     }
 }
+Department.fiscalYear = 2000;
 class ITDepartment extends Department {
     constructor(id, admins) {
         super(id, "IT");
         this.admins = admins;
+    }
+    describe() {
+        console.log("IT Department -ID: " + this.id);
     }
 }
 class AccountingDepartment extends Department {
     constructor(id, reports) {
         super(id, "Accounting");
         this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("No report found");
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error("Please pass in a valid value");
+        }
+        this.addReport(value);
+    }
+    describe() {
+        console.log("Accounting Department -ID: " + this.id);
     }
     addReport(text) {
         this.reports.push(text);
+        this.lastReport = text;
     }
     printReports() {
         console.log(this.reports);
     }
 }
+const employee1 = Department.createEmployee("TemmyG");
+console.log(employee1);
 const it = new ITDepartment("no 1", ["Max"]);
 console.log(it);
 const acctDept = new AccountingDepartment("no 2", []);
+acctDept.describe();
 acctDept.addReport("Something went wrong");
+console.log(acctDept.mostRecentReport);
+acctDept.mostRecentReport = "Welcome home";
+console.log(acctDept.mostRecentReport);
 acctDept.printReports();
 it.describe();
 it.addEmployee("Gabriel");
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
-class Department {
+abstract class Department {
   // name: string;
+  static fiscalYear = 2000;
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     // this.name = n;
   }
 
-  describe(this: Department) {
-    console.log(`Departmnent ${this.id}: ${this.name}`);
+  static createEmployee(name: string) {
+    return { name: name };
   }
+
+  abstract describe(this: Department): void;
 
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -25,27 +28,64 @@ class ITDepartment extends Department {
     super(id, "IT");
     this.admins = admins;
   }
+
+  describe() {
+    console.log("IT Department -ID: " + this.id);
+  }
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value");
+    }
+    this.addReport(value);
+  }
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  describe() {
+    console.log("Accounting Department -ID: " + this.id);
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
   printReports() {
     console.log(this.reports);
   }
 }
 
+const employee1 = Department.createEmployee("TemmyG");
+
+console.log(employee1);
+
 const it = new ITDepartment("no 1", ["Max"]);
 console.log(it);
 
 const acctDept = new AccountingDepartment("no 2", []);
 
+acctDept.describe();
+
 acctDept.addReport("Something went wrong");
+
+console.log(acctDept.mostRecentReport);
+
+acctDept.mostRecentReport = "Welcome home";
+
+console.log(acctDept.mostRecentReport);
 
 acctDept.printReports();
 
